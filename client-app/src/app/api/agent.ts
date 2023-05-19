c1f07710fbc0ec15a5976ba797b5b5cb588e507b
tkarifl <arifoglutarik@gmail.com>
2023-05-19 18:34:42 +0300

    Add mobx, configure store

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,16 +1,15 @@
 import axios, { AxiosResponse } from 'axios';
 import { Activity } from '../models/activity';
-import { resolve } from 'path';
 
-const sleep=(delay:number)=>{
-    return new Promise((resolve)=>{
-        setTimeout(resolve,delay);
+const sleep = (delay: number) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, delay);
     })
 }
 
-axios.defaults.baseURL='http://localhost:5000/api';
+axios.defaults.baseURL = 'http://localhost:5000/api';
 
-axios.interceptors.response.use(async response=>{
+axios.interceptors.response.use(async response => {
     try {
         await sleep(1000);
         return response
@@ -20,24 +19,24 @@ axios.interceptors.response.use(async response=>{
     }
 })
 
-const responseBody=<T> (response:AxiosResponse<T>)=>response.data;
+const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
-const requests={
-    get:<T>(url:string)=>axios.get<T>(url).then(responseBody),
-    post:<T>(url:string,body:{})=>axios.post<T>(url,body).then(responseBody),
-    put:<T>(url:string,body:{})=>axios.put<T>(url,body).then(responseBody),
-    del:<T>(url:string)=>axios.delete<T>(url).then(responseBody),
+const requests = {
+    get: <T>(url: string) => axios.get<T>(url).then(responseBody),
+    post: <T>(url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
+    put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
+    del: <T>(url: string) => axios.delete<T>(url).then(responseBody),
 }
 
-const Activities={
-    list:()=>requests.get<Activity[]>('/activities'),
-    details:(id:string)=>requests.get<Activity>(`/activities/${id}`),
-    create:(activity:Activity)=>requests.post<void>('/activities',activity),
-    update:(activity:Activity)=>requests.put<void>(`/activities/${activity.id}`,activity),
-    delete:(id:string)=>requests.del<void>(`/activities/${id}`)
+const Activities = {
+    list: () => requests.get<Activity[]>('/activities'),
+    details: (id: string) => requests.get<Activity>(`/activities/${id}`),
+    create: (activity: Activity) => requests.post<void>('/activities', activity),
+    update: (activity: Activity) => requests.put<void>(`/activities/${activity.id}`, activity),
+    delete: (id: string) => requests.del<void>(`/activities/${id}`)
 }
 
-const agent={
+const agent = {
     Activities
 }
 
