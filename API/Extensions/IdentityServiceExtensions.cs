9cdbe7b20d2b215a03c4e80fe03c9f470422134c
tkarifl <arifoglutarik@gmail.com>
2023-08-07 21:09:03 +0300

    Add Identity user,migration and services

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,22 @@
+using Domain;
+using Persistence;
+
+namespace API.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddIdentityCore<AppUser>(opt =>
+            {
+                //the default options for passwords are secure enough, so no need to change them
+                //I gave require non alpha numeric prop to true, even though it is true by default, I just wanted to show that we can change them as we wish
+                opt.Password.RequireNonAlphanumeric = true;
+            })
+            .AddEntityFrameworkStores<DataContext>();
+            services.AddAuthentication();
+
+            return services;
+        }
+    }
+}
