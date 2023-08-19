6fc4632d8953898aedcedcc34d41e8aed8ff8604
tkarifl <arifoglutarik@gmail.com>
2023-08-19 19:26:27 +0300

    Complete client-side registration and login

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,7 +1,21 @@
-import { Button, Container, Menu } from "semantic-ui-react";
-import { NavLink } from "react-router-dom";
+import {
+  Button,
+  Container,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  Image,
+  Menu,
+  MenuItem,
+} from "semantic-ui-react";
+import { Link, NavLink } from "react-router-dom";
+import { useStore } from "../stores/store";
+import { observer } from "mobx-react-lite";
 
-export default function NavBar() {
+export default observer(function NavBar() {
+  const {
+    userStore: { user, logout },
+  } = useStore();
   return (
     <Menu inverted fixed="top">
       <Container>
@@ -23,7 +37,25 @@ export default function NavBar() {
             to="/createActivity"
           />
         </Menu.Item>
+        <MenuItem position="right">
+          <Image
+            src={user?.image || "/assets/user.png"}
+            avatar
+            spaced="right"
+          />
+          <Dropdown pointing="top left" text={user?.displayName}>
+            <DropdownMenu>
+              <DropdownItem
+                as={Link}
+                to={`/profile/${user?.username}`}
+                text="My Profile"
+                icon="user"
+              />
+              <DropdownItem onClick={logout} text="Logout" icon="power" />
+            </DropdownMenu>
+          </Dropdown>
+        </MenuItem>
       </Container>
     </Menu>
   );
-}
+});
--- client-app/src/app/stores/commonStore.ts
+++ client-app/src/app/stores/commonStore.ts
@@ -1,15 +1,38 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, reaction } from "mobx";
 import { ServerError } from "../models/serverError";
 
 export default class CommonStore {
     error: ServerError | null = null;
+    token: string | null = localStorage.getItem("jwt");
+    appLoaded = false;
 
     constructor() {
         makeAutoObservable(this);
+
+        // when the observer token has changed, this reaction will trigger, so local storage will match with the mobx state
+        reaction(
+            () => this.token,
+            token => {
+                if (token) {
+                    localStorage.setItem('jwt', token)
+                }
+                else {
+                    localStorage.removeItem('jwt')
+                }
+            }
+        )
     }
 
     setServerError(error: ServerError) {
         this.error = error;
+    }
+
+    setToken = (token: string | null) => {
+        this.token = token;
+    }
+
+    setAppLoaded = () => {
+        this.appLoaded = true;
     }
 
 }
