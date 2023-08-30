7cc83a95120e62a6e63fdbf7b16feef50d421afe
tkarifl <arifoglutarik@gmail.com>
2023-08-30 20:23:21 +0300

    Configure automapper, get related entities

--- Infrastructure/Security/UserAccessor.cs
+++ Infrastructure/Security/UserAccessor.cs
@@ -11,7 +11,6 @@ namespace Infrastructure.Security
         public UserAccessor(IHttpContextAccessor httpContextAccessor)
         {
             _httpContextAccessor = httpContextAccessor;
-
         }
 
         // use httpcontextaccessor, so user object could be used to get the user claims
